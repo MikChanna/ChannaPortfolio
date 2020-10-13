3d2a96661596d54df74598b7c12bf79af47c987f
MikChanna <channa.mik@gmail.com>
2020-10-13 16:49:03 -0400

    added hero nav bar and dummy content

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+
+
+
+return<header class="sticky-shrinknav-header navbar">
+<h1 class="sticky-shrinknav-header-title ">Channa Mik Carlson</h1>
+<ul class="menu align-center sticky-shrinknav-menu">
+  <li><a href="#">Top</a></li>
+  <li><a href="#">About</a></li>
+  <li><a href="#">Portfolio</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+</header>
+}
+
+export default Navbar;
