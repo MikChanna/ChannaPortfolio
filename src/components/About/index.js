e0126967330961ac4126bdf4f71d404d1297928a
MikChanna <channa.mik@gmail.com>
2020-10-19 23:37:48 -0400

    added links to social media and added more to bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ return    <section className = "about" id= "about">
 <div className = "section__div section__div--about">
     <img className= "section__img section__img--about" src = {require("../Images/channainwhite.jpg")} alt="Channa no jacket"/>
 
-    <p className = "section__body section__body--about">  I have a background in non-profits with experience in volunteer management.  The design and aesthetics is what I enjoy most about web development, but I've come to appreciate the logic that works behind the webpages.  I've worked on various teams with an assortment of folks so I am prompt and detailed in communication and can work flexibly with others or on my own.  It is my goal to design pages for all types of
+    <p className = "section__body section__body--about">  I have a background in non-profits with experience in volunteer management where I learned to value transparency and communication.  The design and aesthetics is what I enjoy most about web development, but I've come to appreciate the logic that works behind the webpages.  I've worked on various teams with an assortment of folks so I am prompt and detailed in communication and can work flexibly with others or on my own.  It is my goal to design pages for all types of
         content especially for art, technology, music, and philanthropy.  I am humbly confident, curious, and always
         looking for ways to improve. Click here to view my <a target="_blank" href = "../Images/channa.mik.carlson.resume.pdf">resume.</a> </p>
     
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ function Footer () {
 
     return <footer>
     <div className = "footer__imgdiv">
-        <a className="social-list__link"><img className="footer__img" src = {require("../Images/linkedin.jpeg")} id = "contact"/></a>
-        <a className="social-list__link"><img className="footer__img" src = {require("../Images/github.png")}/></a>
+        <a className="social-list__link" target = "blank" href = "https://www.linkedin.com/in/channamik/"><img className="footer__img" src = {require("../Images/linkedin.jpeg")} id = "contact"/></a>
+        <a className="social-list__link" target = "blank" href = "https://github.com/MikChanna"><img className="footer__img" src = {require("../Images/github.png")}/></a>
      </div>
     <a href="mailto:channa.mik@dgmail.com" className="footer__email">channa.mik@gmail.com</a>
 
