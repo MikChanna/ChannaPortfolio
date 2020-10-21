9d742985dc71e9c903c56ecea88f96f693c4b53f
MikChanna <channa.mik@gmail.com>
2020-10-21 18:20:00 -0400

    fixed image paths

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ function Footer () {
 
     return <footer>
     <div className = "footer__imgdiv">
-        <a className="social-list__link" target = "blank" href = "https://www.linkedin.com/in/channamik/"><img className="footer__img" src = {require("../Images/linkedin.jpeg")} id = "contact" alt= "linkedin logo"/></a>
-        <a className="social-list__link" target = "blank" href = "https://github.com/MikChanna"><img className="footer__img" src = {require("../Images/github.png")} alt= "github logo"/></a>
+        <a className="social-list__link" target = "blank" href = "https://www.linkedin.com/in/channamik/"><img className="footer__img" src = "./assets/Images/linkedin.jpeg" id = "contact" alt= "linkedin logo"/></a>
+        <a className="social-list__link" target = "blank" href = "https://github.com/MikChanna"><img className="footer__img" src = "./assets/Images/github.png" alt= "github logo"/></a>
      </div>
     <a href="mailto:channa.mik@dgmail.com" className="footer__email">channa.mik@gmail.com</a>
 
