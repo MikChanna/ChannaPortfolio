813967b46bae76a029aee1f090c7763c0a4f05fe
MikChanna <channa.mik@gmail.com>
2020-10-20 20:59:27 -0400

    fixed image path

--- src/components/ProjectDiv/index.js
+++ src/components/ProjectDiv/index.js
@@ -3,12 +3,20 @@ import React from 'react';
 
 function ProjectDiv (props) {
 
+let imagePath = "./assets/Images/" + props.image;
+
+console.log(imagePath);
+
+
+
 return  <div className = "section__item--porfolio">
-<a target="blank" href = {props.href}><img src = {require("../Images/BurgerApp.jpg")} alt = {props.name} className = "section__img--portfolio"/> </a>
+<a target="blank" href = {props.href}><img src = {imagePath} alt = {props.name} className = "section__img--portfolio"/> </a>
 
 <p>{props.detail} </p>
 <p className="accent">{props.technology}</p>
 </div>
+
+
 
 }
 
