4d9a8d50c61197b6d7ff99b5283e9e12fb5f6fd2
MikChanna <channa.mik@gmail.com>
2020-11-30 20:55:42 -0500

    adding github link to div

--- src/components/ProjectDiv/index.js
+++ src/components/ProjectDiv/index.js
@@ -11,7 +11,7 @@ console.log(imagePath);
 
 return  <div className = "section__item--porfolio">
 <a target="blank" href = {props.href}><img src = {imagePath} alt = {props.name} className = "section__img--portfolio"/> </a>
-
+<a target="blank" href = {props.github}><p>Github Link</p></a>
 <p>{props.detail} </p>
 <p className="accent">{props.technology}</p>
 </div>
