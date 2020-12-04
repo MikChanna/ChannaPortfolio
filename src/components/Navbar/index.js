65e64b61c2713dd5eae5c66c3160cf98d5a9c03b
MikChanna <channa.mik@gmail.com>
2020-12-04 13:04:07 -0500

    fixed typos and updated resume

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,7 +11,7 @@ return <div>
 <h1 className="sticky-shrinknav-header-title ">Channa Mik Carlson</h1>
 <ul className="menu align-center sticky-shrinknav-menu">
   <li><a href="#about">About</a></li>
-  <li><a href="#porfolio">Portfolio</a></li>
+  <li><a href="#portfolio">Portfolio</a></li>
   <li><a href="#contact">Contact</a></li>
 </ul>
 </header>
