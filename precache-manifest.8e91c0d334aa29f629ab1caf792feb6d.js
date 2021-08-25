a1852c90024f8587cddf4b2c4eed09b1269f337d
MikChanna <channa.mik@gmail.com>
2021-08-25 14:16:57 -0400

    Updates

---
+++ precache-manifest.8e91c0d334aa29f629ab1caf792feb6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09cf057048d1a06cc573fea7e9141f36",
+    "url": "/ChannaPortfolio/index.html"
+  },
+  {
+    "revision": "a2fddcc0f781ecfebdd1",
+    "url": "/ChannaPortfolio/static/css/main.7c76f673.chunk.css"
+  },
+  {
+    "revision": "51b388fe7c63d37d9c55",
+    "url": "/ChannaPortfolio/static/js/2.9921e3c0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ChannaPortfolio/static/js/2.9921e3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fddcc0f781ecfebdd1",
+    "url": "/ChannaPortfolio/static/js/main.4530de4c.chunk.js"
+  },
+  {
+    "revision": "6b0bcdd4e51179b371b0",
+    "url": "/ChannaPortfolio/static/js/runtime-main.57e2e68d.js"
+  },
+  {
+    "revision": "65a398277328b9d5ca153f42d0b7efca",
+    "url": "/ChannaPortfolio/static/media/channaResume.65a39827.pdf"
+  }
+]);
